5b5c317aa60f9fbd9b4fd737ca58abe221206ce0
Exze13 <exze91@yandex.ru>
2023-12-17 19:31:03 +0300

    Second step

--- src/main/java/com/example/demo/controller/PhoneController.java
+++ src/main/java/com/example/demo/controller/PhoneController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.PhoneDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,7 +11,7 @@ import java.util.List;
 @RestController
 public class PhoneController {
 
-    ArrayList<PhoneDTO> phones = new ArrayList<>() {
+    private ArrayList<PhoneDTO> phones = new ArrayList<>() {
         {
             add(new PhoneDTO("7 999 999 99 99"));
         }
@@ -38,5 +40,16 @@ public class PhoneController {
     public PhoneDTO createPhone(@RequestBody PhoneDTO phone) {
         phones.add(phone);
         return phone;
+    }
+
+@DeleteMapping("/phone/{id}")
+@ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete (@PathVariable int id){
+        for (int i = 0; i < phones.size(); i++) {
+            if (phones.get(i).getId() == id){
+                phones.remove(phones.get(i));
+                break;
+            }
+        }
     }
 }
--- src/main/java/com/example/demo/dto/PhoneDTO.java
+++ src/main/java/com/example/demo/dto/PhoneDTO.java
@@ -1,20 +1,34 @@
 package com.example.demo.dto;
 
 public class PhoneDTO {
+
+    private static int lastId = 0;
     private String phone;
+    private int id;
+
+
+    public PhoneDTO(String phone) {
+        this.phone = phone;
+        lastId++;
+        this.id = lastId;
+    }
+    public PhoneDTO() {
+        lastId++;
+        this.id = lastId;
+    }
+
 
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
-    public PhoneDTO(String phone) {
-        this.phone = phone;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public PhoneDTO() {
-    }
 }
